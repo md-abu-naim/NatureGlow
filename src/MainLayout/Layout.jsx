1bc9cdaacd0e822fdad5f56ecdd5aa163f59963c
abunaim34 <farhanadnanfarabi@gmail.com>
2025-06-18 21:45:13 +0600

    footer resposive

--- src/MainLayout/Layout.jsx
+++ src/MainLayout/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
         <div className='bg-white text-black font-serif'>
             <Navber />
 
-            <div className='min-h-[calc(100vh-250px)]'>
+            <div className='min-h-[calc(100vh-335px)]'>
                 <Outlet />
             </div>
 
