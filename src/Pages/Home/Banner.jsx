eeca2258c0c08e9afc4312e4f6956fa43a698a78
abunaim34 <farhanadnanfarabi@gmail.com>
2025-06-18 22:10:58 +0600

    banner content add

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div>
-            <h6>Hey, Welcome to</h6>
-            <h1>NatureGlow</h1>
-            <p>Discover our handcrafted organic skincare products made with botanical ingredients. Free from toxins, full of love — because your skin deserves the best nature has to offer.</p>
-            <Link to={'/shop'}>SHOP NOW</Link>
+        <div className='flex justify-between items-center border bg-[url("/bg.jpg")] '>
+            <div>
+                <h6>Hey, Welcome to</h6>
+                <h1>NatureGlow</h1>
+                <p>Discover our handcrafted organic skincare products made with botanical ingredients. Free from toxins, full of love — because your skin deserves the best nature has to offer.</p>
+                <Link to={'/shop'}>SHOP NOW</Link>
+            </div>
+            <div>
+                <img src="/banner.png" alt="" />
+            </div>
         </div>
     );
 };
