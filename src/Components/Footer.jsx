356a43fe716049022f2ccddbad14fc90ec08fef7
abunaim34 <farhanadnanfarabi@gmail.com>
2025-06-18 20:21:22 +0600

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,11 @@
 const Footer = () => {
     return (
         <div>
-            footer x
+            footer x 
+            Full-stack Web Developer with a passion for crafting modern, responsive web applications. Skilled in HTML, CSS, Tailwind CSS, JavaScript, React, Firebase Auth, Node.js, Express.js, and MongoDB. I’m here to grow, learn from experienced freelancers, and share value with the Fiverr community. Let’s connect, inspire, and build together!
+Hi! I'm a passionate frontend developer specializing in modern, responsive UI templates using HTML & CSS, or React and Tailwind CSS. I craft clean, minimal, and easy-to-customize website templates for startups, portfolios, agencies, and more. All my templates come with clean code and detailed documentation. Let’s make the web more beautiful—together!
+Frontend developer crafting clean, modern UI templates with HTML, CSS, React & Tailwind. My designs are responsive, minimal, and easy to customize. All templates come with clean code and clear docs—built to help you launch faster and look great online.
+I'm a passionate front-end developer with expertise in HTML, CSS, Tailwind CSS, and React.js. I focus on creating clean, responsive, and user-friendly website templates. As a new author, I’m committed to providing high-quality code and professional design for clients around the world
         </div>
     );
 };
