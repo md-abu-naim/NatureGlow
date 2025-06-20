34c885fbf0f91eda793cbf808eceaff59e1fcfe2
abunaim34 <farhanadnanfarabi@gmail.com>
2025-06-20 09:56:19 +0600

    navber desgin update

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -13,7 +13,7 @@ const Navber = () => {
     </>
 
     return (
-        <div className="navbar px-5 bg-yellow-50 text-black shadow-sm">
+        <div className="navbar px-10 bg-yellow-50 text-black shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
@@ -22,25 +22,34 @@ const Navber = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li>{navLinks}</li>
-
+                        {navLinks}
                     </ul>
                 </div>
-                <a className="text-3xl"> <img className='w-[250px]' src="/NatureGlowLogo.png" alt="" /> </a>
+                <NavLink to="/" className="text-3xl"> <img className='w-[250px]' src="/NatureGlowLogo.png" alt="" /> </NavLink>
             </div>
             <div className="navbar-center hidden md:flex">
                 <ul className="menu menu-horizontal px-1">
                     {navLinks}
                 </ul>
             </div>
-            <div className="navbar-end flex gap-4 relative">
-                <button className="bg-auto text-2xl"><VscAccount /></button>
-                <button className="bg-auto w-[50px] h-[50px] text-2xl"><FaCartPlus />
+            <div className="navbar-end gap-2 relative">
+                <button className="text-2xl  hover:text-green-600 transition-colors"><VscAccount /></button>
+                {/* <button className="bg-auto w-[50px] h-[50px] text-2xl"><FaCartPlus />
                     <div className="absolute  right-0 top-0">12</div>
-                </button>
+                </button> */}
+                <div className="relative">
+                    <button className="text-2xl hover:text-green-600 transition-colors">
+                        <FaCartPlus />
+                    </button>
+                    <span className="absolute -top-2 -right-2 bg-green-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
+                        12
+                    </span>
+                </div>
             </div>
         </div>
     );
 };
 
 export default Navber;
+
+
