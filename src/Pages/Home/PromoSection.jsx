9a9e32d5a7864a40341075e5f15e2b71d2f8a3d0
abunaim34 <farhanadnanfarabi@gmail.com>
2025-06-22 22:06:30 +0600

    promo section code update

--- src/Pages/Home/PromoSection.jsx
+++ src/Pages/Home/PromoSection.jsx
@@ -2,20 +2,16 @@ import { Link } from "react-router-dom";
 
 const PromoSection = () => {
     return (
-        <div>
-
-            <div className="flex flex-col md:flex-row items-center bg-green-50 rounded-md p-5 md:p-16 my-16 shadow-lg">
-                <div className="md:w-1/2 mb-8 md:mb-0 md:pr-12">
-                    <h2 className="text-4xl font-extrabold text-green-800 mb-4">Discover the Secret to Radiant Skin</h2>
-                    <p className="text-green-700 mb-6 leading-relaxed">Pamper yourself with our exclusive spa collection made from
-                        natural ingredients, designed to rejuvenate your skin and soul.</p>
-                    <Link to={'/shop'} className='bg-green-600 text-white px-8 py-3 rounded-full font-semibold shadow hover:bg-green-700 transition duration-300' >SHOP NOW</Link>
-                </div>
-                <div className="md:w-1/2 flex justify-center">
-                    <img className="rounded-3xl w-full max-h-96 shadow-lg border border-green-200" src="/Promo.png" alt="" />
-                </div>
+        <div className="flex flex-col md:flex-row items-center bg-green-50 rounded-md p-5 md:p-16 my-16 shadow-lg">
+            <div className="md:w-1/2 mb-8 md:mb-0 md:pr-12">
+                <h2 className="text-4xl font-extrabold text-green-800 mb-4">Discover the Secret to Radiant Skin</h2>
+                <p className="text-green-700 mb-6 leading-relaxed">Pamper yourself with our exclusive spa collection made from
+                    natural ingredients, designed to rejuvenate your skin and soul.</p>
+                <Link to={'/shop'} className='bg-green-600 text-white px-8 py-3 rounded-full font-semibold shadow hover:bg-green-700 transition duration-300' >SHOP NOW</Link>
             </div>
-
+            <div className="md:w-1/2 flex justify-center">
+                <img className="rounded-3xl w-full max-h-96 shadow-lg border border-green-200" src="/Promo.png" alt="" />
+            </div>
         </div>
     );
 };
