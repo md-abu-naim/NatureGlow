e6067b3ece836cb2d7034807fa80277e2ececcff
abunaim34 <farhanadnanfarabi@gmail.com>
2025-06-22 21:45:35 +0600

    Team section data displayed

--- src/Pages/About/Team.jsx
+++ src/Pages/About/Team.jsx
@@ -1,14 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Team = () => {
+    const [team, setTeam] = useState([])
+
+    useEffect(() => {
+        axios.get('/Team.json')
+        .then(res => setTeam(res.data))
+    },[])
     return (
         <section className="bg-green-100 py-12 px-6 rounded-2xl">
             <h2 className="text-3xl font-bold text-green-800 text-center mb-8">Meet the Team</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-center">
-                <div className="bg-green-50 rounded-xl p-6 shadow">
-                    <img className="w-24 h-24 mx-auto rounded-full mb-4 object-cover border border-green-200" src="https://i.postimg.cc/B6PjqBMM/Lip-Care.png" alt="" />
-                    <h4 className="font-semibold text-green-700">Name</h4>
-                    <p className="text-sm text-gray-500">Founder & Formulator</p>
-                </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 text-center">
+                {
+                    team?.map(t => <div key={t.id} className="bg-green-50 rounded-xl p-6 shadow">
+                    <img className="w-24 h-24 mx-auto rounded-full mb-4 object-cover border border-green-200" src={t.image} alt="" />
+                    <h4 className="font-semibold text-green-700">{t.name}</h4>
+                    <p className="text-sm text-gray-500">{t.role}</p>
+                </div>)
+                }
             </div>
         </section>
     );
