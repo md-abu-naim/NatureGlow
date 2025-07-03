8da44ddfd5cee90e5347d93f10e5566809162050
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-03 20:20:14 +0600

    firebase config file

--- src/Firebase/Firebasae.config.js
+++ src/Firebase/Firebasae.config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,28 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
-
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  // ...
-  apiKey: "AIzaSyBaI9W2gqPlZbPK0mzQcG2GpcsOix5AHoM",
-  authDomain: "natureglow-740e8.firebaseapp.com",
-  projectId: "natureglow-740e8",
-  storageBucket: "natureglow-740e8.firebasestorage.app",
-  messagingSenderId: "166704578722",
-  appId: "1:166704578722:web:c753e1725f5f54a12f17fb",
-  measurementId: "G-GDLVTFYB47"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export { auth, analytics };
