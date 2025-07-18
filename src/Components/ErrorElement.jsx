3bd6cd1acac64c69c6cf2a83ca36839a5e9a11f4
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-18 22:36:54 +0600

    error page tablet responsive

--- src/Components/ErrorElement.jsx
+++ src/Components/ErrorElement.jsx
@@ -6,12 +6,8 @@ const NotFound = () => {
     <div className="min-h-screen flex flex-col md:flex-row items-center justify-center md:gap-16 px-6 py-12">
       <div className="text-center">
         <h1 className="text-9xl font-extrabold text-green-600 drop-shadow-md">404</h1>
-        <p className="mt-4 text-2xl md:text-3xl font-semibold text-gray-300">
-          Page Not Found
-        </p>
-        <p className="mt-2 text-md text-gray-400">
-          The page you’re looking for doesn’t exist or has been moved.
-        </p>
+        <p className="mt-4 text-2xl md:text-3xl font-semibold text-gray-300">Page Not Found</p>
+        <p className="mt-2 text-md text-gray-400">The page you’re looking for doesn’t exist or has been moved. </p>
 
         <div className="mt-6">
           <Link
