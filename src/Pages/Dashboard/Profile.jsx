f9ebb523dd67f6848ec7aaf0a1c3ba7407fbc1f5
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-23 22:32:41 +0600

    profile page update profile modal funtionlity

--- src/Pages/Dashboard/Profile.jsx
+++ src/Pages/Dashboard/Profile.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Profile = () => {
-  const { user } = useAuth();
+  const { user, updateUser, setUser } = useAuth();
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleUpdateUser = (e) => {
+    e.preventDefault()
+    const name = e.target.name.value
+    const photoURL = e.target.photoURL.value
+    updateUser(name, photoURL)
+    setUser({
+      ...user,
+      displayName: name,
+      photoURL: photoURL
+    })
+    Swal.fire({
+      title: "Updated successfully!",
+      icon: "success",
+      draggable: true,
+      timer: 1500,
+      background: '#dcfce7',
+    });
+    setOpenModal(false)
+  }
 
   return (
     <div>
@@ -18,7 +41,7 @@ const Profile = () => {
             <p className="text-gray-700 text-sm"><span className="font-semibold">User ID: </span> {user?.uid}</p>
 
             <div className={`flex flex-col md:flex-row justify-center gap-4 ${user?.role === 'admin' ? 'my-6' : 'mt-6'}`}>
-              <button className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md transition">Update Profile</button>
+              <button onClick={() => setOpenModal(true)} className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md transition">Update Profile</button>
               <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 px-6 py-2 rounded-md border border-green-300">Change Password</button>
             </div>
 
@@ -35,42 +58,30 @@ const Profile = () => {
         </div>
       </div>
 
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-green-100 w-full max-w-md rounded-lg p-6 shadow-lg border border-green-200">
-          <h2 className="text-2xl font-bold text-green-700 mb-4 text-center">Update Profile</h2>
-          <form className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Name</label>
-              <input
-                type="text"
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-                required
-              />
+      {
+        openModal && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="bg-green-100 w-full max-w-md rounded-lg p-6 shadow-lg border border-green-300">
+              <h2 className="text-2xl font-bold text-green-800 mb-4 text-center">Update Profile</h2>
+              <form onSubmit={handleUpdateUser} className="space-y-4">
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">Name</label>
+                  <input className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400" placeholder={user?.displayName} type="text" name="name" required />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">Photo</label>
+                  <input className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400" type='file' name="photoURL" />
+                </div>
+
+                <div className="flex justify-end gap-4 p-2">
+                  <button onClick={() => setOpenModal(false)} type="button" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">Cansel</button>
+                  <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">Save</button>
+                </div>
+              </form>
             </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Photo URL</label>
-              <input
-                type="text"
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
-            </div>
-            <div className="flex justify-end gap-4 pt-2">
-              <button
-                type="button"
-                className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400"
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
-              >
-                Save
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+          </div>
+        )
+      }
     </div>
   );
 };
