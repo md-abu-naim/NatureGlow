d1124e36b4ef613b7f22181ec7f7e341d75f12ae
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-23 22:51:34 +0600

    profile page change password profile modal funtionality complete

--- src/Pages/Dashboard/Profile.jsx
+++ src/Pages/Dashboard/Profile.jsx
@@ -5,7 +5,8 @@ import { FaLock, FaUnlock } from "react-icons/fa";
 
 const Profile = () => {
   const { user, updateUser, setUser } = useAuth();
-  const [activeUpdateModal, setActiveUpdateModal] = useState(false)
+  const [openUpdateModal, setOpenUpdateModal] = useState(false)
+  const [openPassModal, setOpenPassModal] = useState(false)
   const [showOldPass, setShowOldPass] = useState(false)
   const [newPass, setNewPass] = useState(false)
   const [confirmPass, setConfirmPass] = useState(false)
@@ -27,7 +28,7 @@ const Profile = () => {
       timer: 1500,
       background: '#dcfce7',
     });
-    setActiveUpdateModal(false)
+    setOpenUpdateModal(false)
   }
 
   return (
@@ -45,8 +46,8 @@ const Profile = () => {
             <p className="text-gray-700 text-sm"><span className="font-semibold">User ID: </span> {user?.uid}</p>
 
             <div className={`flex flex-col md:flex-row justify-center gap-4 ${user?.role === 'admin' ? 'my-6' : 'mt-6'}`}>
-              <button onClick={() => setActiveUpdateModal(true)} className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md transition">Update Profile</button>
-              <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 px-6 py-2 rounded-md border border-green-300">Change Password</button>
+              <button onClick={() => setOpenUpdateModal(true)} className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md transition">Update Profile</button>
+              <button onClick={() => setOpenPassModal(true)} className="bg-gray-100 hover:bg-gray-200 text-gray-700 px-6 py-2 rounded-md border border-green-300">Change Password</button>
             </div>
 
             {/* Role Update Request Button */}
@@ -63,7 +64,7 @@ const Profile = () => {
       </div>
 
       {
-        activeUpdateModal && (
+        openUpdateModal && (
           <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div className="bg-green-100 w-full max-w-md rounded-lg p-6 shadow-lg border border-green-300">
               <h2 className="text-2xl font-bold text-green-800 mb-4 text-center">Update Profile</h2>
@@ -78,7 +79,7 @@ const Profile = () => {
                 </div>
 
                 <div className="flex justify-end gap-4 p-2">
-                  <button onClick={() => setActiveUpdateModal(false)} type="button" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">Cansel</button>
+                  <button onClick={() => setOpenUpdateModal(false)} type="button" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">Cansel</button>
                   <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">Save</button>
                 </div>
               </form>
@@ -87,54 +88,58 @@ const Profile = () => {
         )
       }
 
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="bg-green-100 w-full max-w-lg rounded-lg p-6 shadow-lg border border-green-300">
-          <h2 className="text-2xl font-bold text-green-800 mb-4 text-center">Change Password</h2>
-          <form onSubmit={handleUpdateUser} className="space-y-4">
-            <div>
-              <label className="text-sm font-medium text-green-700 mb-1">Current Password</label>
-              <div className="relative">
-                <input name="password" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={showOldPass ? 'text' : 'password'} required placeholder="••••••••" />
-                <span onClick={() => setShowOldPass(!showOldPass)} title="Show Password" className="cursor-pointer">
-                  {
-                    showOldPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
-                      <FaLock className="absolute right-4 top-3.5 text-green-400" />
-                  }
-                </span>
-              </div>
-            </div>
-            <div>
-              <label className="text-sm font-medium text-green-700 mb-1">New Password</label>
-              <div className="relative">
-                <input name="password" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={newPass ? 'text' : 'password'} required placeholder="••••••••" />
-                <span onClick={() => setNewPass(!newPass)} title="Show Password" className="cursor-pointer">
-                  {
-                    newPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
-                      <FaLock className="absolute right-4 top-3.5 text-green-400" />
-                  }
-                </span>
-              </div>
-            </div>
-            <div>
-              <label className="text-sm font-medium text-green-700 mb-1">Confirm Password</label>
-              <div className="relative">
-                <input name="password" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={confirmPass ? 'text' : 'password'} required placeholder="••••••••" />
-                <span onClick={() => setConfirmPass(!confirmPass)} title="Show Password" className="cursor-pointer">
-                  {
-                    confirmPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
-                      <FaLock className="absolute right-4 top-3.5 text-green-400" />
-                  }
-                </span>
-              </div>
-            </div>
+      {
+        openPassModal && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="bg-green-100 w-full max-w-lg rounded-lg p-6 shadow-lg border border-green-300">
+              <h2 className="text-2xl font-bold text-green-800 mb-4 text-center">Change Password</h2>
+              <form onSubmit={handleUpdateUser} className="space-y-4">
+                <div>
+                  <label className="text-sm font-medium text-green-700 mb-1">Current Password</label>
+                  <div className="relative">
+                    <input name="password" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={showOldPass ? 'text' : 'password'} required placeholder="••••••••" />
+                    <span onClick={() => setShowOldPass(!showOldPass)} title="Show Password" className="cursor-pointer">
+                      {
+                        showOldPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
+                          <FaLock className="absolute right-4 top-3.5 text-green-400" />
+                      }
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <label className="text-sm font-medium text-green-700 mb-1">New Password</label>
+                  <div className="relative">
+                    <input name="password" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={newPass ? 'text' : 'password'} required placeholder="••••••••" />
+                    <span onClick={() => setNewPass(!newPass)} title="Show Password" className="cursor-pointer">
+                      {
+                        newPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
+                          <FaLock className="absolute right-4 top-3.5 text-green-400" />
+                      }
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <label className="text-sm font-medium text-green-700 mb-1">Confirm Password</label>
+                  <div className="relative">
+                    <input name="password" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={confirmPass ? 'text' : 'password'} required placeholder="••••••••" />
+                    <span onClick={() => setConfirmPass(!confirmPass)} title="Show Password" className="cursor-pointer">
+                      {
+                        confirmPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
+                          <FaLock className="absolute right-4 top-3.5 text-green-400" />
+                      }
+                    </span>
+                  </div>
+                </div>
 
-            <div className="flex justify-end gap-4 p-2">
-              <button type="button" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">Cansel</button>
-              <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">Update Password</button>
+                <div className="flex justify-end gap-4 p-2">
+                  <button onClick={() => setOpenPassModal(false)} type="button" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">Cansel</button>
+                  <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">Update Password</button>
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
-      </div>
+          </div>
+        )
+      }
     </div>
   );
 };
