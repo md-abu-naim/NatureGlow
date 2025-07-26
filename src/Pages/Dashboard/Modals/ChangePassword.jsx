63edf157ba56f123666573390662fc6c16084f9d
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 22:04:53 +0600

    password validation is successfully

--- src/Pages/Dashboard/Modals/ChangePassword.jsx
+++ src/Pages/Dashboard/Modals/ChangePassword.jsx
@@ -7,60 +7,94 @@ const ChangePassword = ({ setOpenPassModal }) => {
     const [showOldPass, setShowOldPass] = useState(false)
     const [newPass, setNewPass] = useState(false)
     const [confirmPass, setConfirmPass] = useState(false)
-    const { passwordUpdate, reauthencticateUser } = useAuth()
+    const { passwordUpdate, reauthencticateUser, user } = useAuth()
+
+
+    const isPasswordProvider = user?.providerData?.some(
+        (provider) => provider.providerId === 'password'
+    )
+
 
     const handleChangePassword = (e) => {
         e.preventDefault()
         const form = e.target
-        const currentPass = form.currentPass.value
+        // const currentPass = form.currentPass.value
         const newPassword = form.newPass.value
         const confirmPass = form.confirmPass.value
-        console.log(form, currentPass, newPassword, confirmPass);
-        if (currentPass == newPassword) {
-            return Swal.fire("New password can't be same as current password");
-        }
+        console.log(form, newPassword, confirmPass);
         if (newPassword !== confirmPass) {
             return Swal.fire("New password and confirm password must match");
         }
-        reauthencticateUser(currentPass)
-            .then(() => {
-                return passwordUpdate(newPassword)
-            })
-            .then(() => {
-                Swal.fire({
-                    title: "Password updated successfully!",
-                    icon: "success",
-                    timer: 1500,
-                    background: "#dcfce7",
+
+        if (isPasswordProvider) {
+            const currentPass = form.currentPass.value
+            if (currentPass == newPassword) {
+                return Swal.fire("New password can't be same as current password");
+            }
+            reauthencticateUser(currentPass)
+                .then(() => {
+                    return passwordUpdate(newPassword)
+                })
+                .then(() => {
+                    Swal.fire({
+                        title: "Password updated successfully!",
+                        icon: "success",
+                        timer: 1500,
+                        background: "#dcfce7",
+                    });
+                    setOpenPassModal(false)
+                })
+                .catch((error) => {
+                    console.error("Error updating password", error);
+                    Swal.fire({
+                        title: "Please type your valid password",
+                        text: error.message,
+                        icon: "error",
+                    });
                 });
-                e.reset()
-            })
-            .catch((error) => {
-                console.error("Error updating password", error);
-                Swal.fire({
-                    title: "Please type your valid password",
-                    text: error.message,
-                    icon: "error",
+        }
+        else {
+            passwordUpdate(newPassword)
+                .then(() => {
+                    Swal.fire({
+                        title: "Password updated successfully!",
+                        icon: "success",
+                        timer: 1500,
+                        background: "#dcfce7",
+                    });
+                    setOpenPassModal(false)
+                })
+                .catch((error) => {
+                    console.error("Error updating password", error);
+                    Swal.fire({
+                        title: "Please type your valid password",
+                        text: error.message,
+                        icon: "error",
+                    });
                 });
-            });
+        }
     }
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div className="bg-green-100 w-full max-w-lg rounded-lg p-6 shadow-lg border border-green-300">
-                <h2 className="text-2xl font-bold text-green-800 mb-4 text-center">Change Password</h2>
+                <h2 className="text-2xl font-bold text-green-800 mb-4 text-center">{isPasswordProvider ? "Change Password" : 'Update Password'}</h2>
                 <form onSubmit={handleChangePassword} className="space-y-4">
-                    <div>
-                        <label className="text-sm font-medium text-green-700 mb-1">Current Password</label>
-                        <div className="relative">
-                            <input name="currentPass" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={showOldPass ? 'text' : 'password'} required placeholder="••••••••" />
-                            <span onClick={() => setShowOldPass(!showOldPass)} title="Show Password" className="cursor-pointer">
-                                {
-                                    showOldPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
-                                        <FaLock className="absolute right-4 top-3.5 text-green-400" />
-                                }
-                            </span>
-                        </div>
-                    </div>
+                    {
+                        isPasswordProvider && (
+                            <div>
+                                <label className="text-sm font-medium text-green-700 mb-1">Current Password</label>
+                                <div className="relative">
+                                    <input name="currentPass" className="w-full px-4 py-3 border border-green-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500" type={showOldPass ? 'text' : 'password'} required placeholder="••••••••" />
+                                    <span onClick={() => setShowOldPass(!showOldPass)} title="Show Password" className="cursor-pointer">
+                                        {
+                                            showOldPass ? <FaUnlock className="absolute right-4 top-3.5 text-green-400" /> :
+                                                <FaLock className="absolute right-4 top-3.5 text-green-400" />
+                                        }
+                                    </span>
+                                </div>
+                            </div>
+                        )
+                    }
                     <div>
                         <label className="text-sm font-medium text-green-700 mb-1">New Password</label>
                         <div className="relative">
