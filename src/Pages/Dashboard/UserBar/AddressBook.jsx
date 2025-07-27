8808ea8bcb216ebb58c653c0f860e95ad59e9fd3
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-27 20:43:43 +0600

    addressBook page title section

--- src/Pages/Dashboard/UserBar/AddressBook.jsx
+++ src/Pages/Dashboard/UserBar/AddressBook.jsx
@@ -2,7 +2,10 @@
 const AddressBook = () => {
     return (
         <div>
-            adsfjs
+            <section className='bg-green-100 py-5 text-center rounded-lg'>
+                <h1 className='text-4xl font-extrabold text-green-800'>Your Saved Addresses</h1>
+                <p className='text-green-600 text-sm mt-2'>Easily manage your shipping addresses in one place. Add, update, or delete locations to ensure smooth and fast deliveries every time you shop.</p>
+            </section>
         </div>
     );
 };
