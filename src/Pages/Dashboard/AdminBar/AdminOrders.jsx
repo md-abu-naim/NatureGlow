ac9d5b6877623271a0b644ab900870db63241cb1
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-28 21:19:00 +0600

    admin orders page order shipped card

--- src/Pages/Dashboard/AdminBar/AdminOrders.jsx
+++ src/Pages/Dashboard/AdminBar/AdminOrders.jsx
@@ -1,4 +1,4 @@
-import { BsCartX } from "react-icons/bs";
+import { BsCartX, BsTruck } from "react-icons/bs";
 
 const AdminOrders = () => {
     return (
@@ -19,6 +19,17 @@ const AdminOrders = () => {
                         <BsCartX className="text-3xl" />
                     </div>
                 </div>
+                <div className="bg-gradient-to-br from-green-100 to-green-50 p-6 rounded-2xl shadow-md hover:shadow-xl ransition duration-300 ease-in-out group relative flex items-center justify-between">
+                    <div>
+                        <h3 className="text-xl font-bold text-green-800 group-hover:text-green-900 transition">Order Shipped</h3>
+                        <p className="text-3xl font-semibold text-gray-800 mt-1">250</p>
+                        <p className="text-sm text-green-600 mt-1">Successfully shipped to customers</p>
+                    </div>
+                    <div className="bg-green-200 text-green-800 p-4 rounded-full shadow-inner group-hover:bg-green-300 transition">
+                        <BsTruck className="text-3xl" />
+                    </div>
+                </div>
+                
             </section>
         </div>
     );
