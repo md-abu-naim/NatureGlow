0a38ed28b4b4bbf645edca6bb8cf0455898ae729
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-29 22:10:17 +0600

    order details page fully responsive

--- src/Pages/Dashboard/OrderTracking.jsx
+++ src/Pages/Dashboard/OrderTracking.jsx
@@ -9,9 +9,9 @@ const OrderTracking = ({ status = 'in progress' }) => {
     }[status.toLowerCase()] || 0
     return (
         <div className="bg-green-50 p-4 mt-6 rounded-lg">
-            <div className="flex justify-between items-center pb-10">
+            <div className="flex flex-col md:flex-row justify-between items-center pb-10">
                 <h3 className="text-xl font-bold text-green-800">Order Tracking</h3>
-                <div className='flex gap-2'>
+                <div className='flex gap-2 mt-2 lg:mt-0'>
                     {/* <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm ${product.paymentStatus === 'Paid' ? 'bg-green-100 text-green-700' : product.paymentStatus === 'Unpaid' ? 'bg-red-100 text-red-700' : 'bg-blue-100 text-blue-700'}`}>
                                         paid
                                     </span>
@@ -22,10 +22,11 @@ const OrderTracking = ({ status = 'in progress' }) => {
                                         }`}>
                                         {product.orderStatus}
                                     </span> */}
-                    <span className='bg-green-100 text-green-700 py-1 px-2 shadow rounded'>Paid</span>
-                    <span className='border border-yellow-300 rounded-full text-yellow-400 p-1'>In Progrss</span>
+                    <span className='bg-green-100 text-green-700 py-1 px-2 shadow rounded text-sm font-medium'>Paid</span>
+                    <span className='border border-yellow-300 rounded-full text-yellow-500 px-3 py-1 text-sm font-medium shadow'>In Progrss</span>
                 </div>
             </div>
+
 
             {/* Order tracking */}
             <Box sx={{ width: '100%', py: 3 }}>
