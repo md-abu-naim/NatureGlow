06b52e91085561e37855ba1207177b409bfcef8a
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-29 22:28:10 +0600

    single order fetching

--- src/Pages/Dashboard/OrderDetails.jsx
+++ src/Pages/Dashboard/OrderDetails.jsx
@@ -1,57 +1,64 @@
 import { FaCcMastercard, FaEdit, FaTrash } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLoaderData, useParams } from "react-router-dom";
 import OrderTracking from "./OrderTracking";
 
 const OrderDetails = () => {
-    const products = [
-        {
-            "_id": 1,
-            "image": "https://i.ibb.co/0YhPzTX/product1.jpg",
-            "name": "Herbal Aloe Face Wash",
-            "date": "2025-07-20T10:15:00Z",
-            "orderStatus": "Delivered",
-            "paymentStatus": "Paid",
-            "totalPrice": 2499
-        },
-        {
-            "_id": 2,
-            "image": "https://i.ibb.co/5vQFdxN/product2.jpg",
-            "name": "Coconut Hair Oil",
-            "date": "2025-07-22T09:45:00Z",
-            "orderStatus": "Shipped",
-            "paymentStatus": "Paid",
-            "totalPrice": 1890
-        },
-        {
-            "_id": 3,
-            "image": "https://i.ibb.co/J5Phn8b/product3.jpg",
-            "name": "Rose Body Lotion",
-            "date": "2025-07-25T12:10:00Z",
-            "orderStatus": "In Progress",
-            "paymentStatus": "Unpaid",
-            "totalPrice": 2785
-        },
-        {
-            "_id": 4,
-            "image": "https://i.ibb.co/SJ7Z0KR/product4.jpg",
-            "name": "Lip Balm (Strawberry)",
-            "date": "2025-07-23T16:00:00Z",
-            "orderStatus": "Cancelled",
-            "paymentStatus": "Refunded",
-            "totalPrice": 1540
-        },
-        {
-            "_id": 5,
-            "image": "https://i.ibb.co/Ln6dTn9/product5.jpg",
-            "name": "Turmeric Clay Mask",
-            "date": "2025-07-18T14:30:00Z",
-            "orderStatus": "In Progress",
-            "paymentStatus": "Paid",
-            "totalPrice": 3200
-        }
-    ]
+    const {id} = useParams()
+    const products = useLoaderData()
 
-    const total = Math.round(products.reduce((sum, item) => sum + item.totalPrice, 0) * 100) / 100;
+    const product = products.find(p => p.id === id)
+
+    const {paymentStatus, orderStatus} = product || {}
+    console.log(id, product);
+    // const products = [
+    //     {
+    //         "_id": 1,
+    //         "image": "https://i.ibb.co/0YhPzTX/product1.jpg",
+    //         "name": "Herbal Aloe Face Wash",
+    //         "date": "2025-07-20T10:15:00Z",
+    //         "orderStatus": "Delivered",
+    //         "paymentStatus": "Paid",
+    //         "totalPrice": 2499
+    //     },
+    //     {
+    //         "_id": 2,
+    //         "image": "https://i.ibb.co/5vQFdxN/product2.jpg",
+    //         "name": "Coconut Hair Oil",
+    //         "date": "2025-07-22T09:45:00Z",
+    //         "orderStatus": "Shipped",
+    //         "paymentStatus": "Paid",
+    //         "totalPrice": 1890
+    //     },
+    //     {
+    //         "_id": 3,
+    //         "image": "https://i.ibb.co/J5Phn8b/product3.jpg",
+    //         "name": "Rose Body Lotion",
+    //         "date": "2025-07-25T12:10:00Z",
+    //         "orderStatus": "In Progress",
+    //         "paymentStatus": "Unpaid",
+    //         "totalPrice": 2785
+    //     },
+    //     {
+    //         "_id": 4,
+    //         "image": "https://i.ibb.co/SJ7Z0KR/product4.jpg",
+    //         "name": "Lip Balm (Strawberry)",
+    //         "date": "2025-07-23T16:00:00Z",
+    //         "orderStatus": "Cancelled",
+    //         "paymentStatus": "Refunded",
+    //         "totalPrice": 1540
+    //     },
+    //     {
+    //         "_id": 5,
+    //         "image": "https://i.ibb.co/Ln6dTn9/product5.jpg",
+    //         "name": "Turmeric Clay Mask",
+    //         "date": "2025-07-18T14:30:00Z",
+    //         "orderStatus": "In Progress",
+    //         "paymentStatus": "Paid",
+    //         "totalPrice": 3200
+    //     }
+    // ]
+
+    const total = Math.round(product.products.reduce((sum, item) => sum + item.totalPrice, 0) * 100) / 100;
     return (
         <div>
             <section className='bg-green-100 py-5 text-center rounded-lg'>
@@ -63,7 +70,7 @@ const OrderDetails = () => {
                 <div className="col-span-2">
                     <div>
                         {/* <OrderTracking /> */}
-                        <OrderTracking />
+                        <OrderTracking orderStatus={orderStatus} paymentStatus={paymentStatus} />
 
                         {/* Order List */}
                         <section className=" mt-6 overflow-x-auto rounded-lg shadow-lg border border-green-300">
@@ -83,7 +90,7 @@ const OrderDetails = () => {
                                 </thead>
                                 <tbody className="text-gray-700 divide-y divide-green-100">
                                     {
-                                        products?.map((product, i) => (
+                                        product.products?.map((product, i) => (
                                             <tr key={product.id} className="hover:bg-green-100 transition-all">
                                                 <td className="px-4 py-3 font-semibold">{i + 1}</td>
                                                 <td className="px-4 py-3 flex items-center gap-1">
--- src/Pages/Dashboard/OrderTracking.jsx
+++ src/Pages/Dashboard/OrderTracking.jsx
@@ -1,7 +1,7 @@
 import { Box, Stepper, Step, StepLabel } from '@mui/material';
 
 const steps = ['In Progress', 'Shipped', 'Delivered']
-const OrderTracking = ({ status = 'in progress' }) => {
+const OrderTracking = ({ status = 'in progress', paymentStatus, orderStatus }) => {
     const stepIndex = {
         'in progress': 0,
         shipped: 1,
@@ -12,18 +12,18 @@ const OrderTracking = ({ status = 'in progress' }) => {
             <div className="flex flex-col md:flex-row justify-between items-center pb-10">
                 <h3 className="text-xl font-bold text-green-800">Order Tracking</h3>
                 <div className='flex gap-2 mt-2 lg:mt-0'>
-                    {/* <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm ${product.paymentStatus === 'Paid' ? 'bg-green-100 text-green-700' : product.paymentStatus === 'Unpaid' ? 'bg-red-100 text-red-700' : 'bg-blue-100 text-blue-700'}`}>
+                    <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm ${paymentStatus === 'Paid' ? 'bg-green-100 text-green-700' : paymentStatus === 'Unpaid' ? 'bg-red-100 text-red-700' : 'bg-blue-100 text-blue-700'}`}>
                                         paid
                                     </span>
-                                    <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm
+                                    {/* <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm
                                             ${product.orderStatus === 'In Progress' ? 'bg-yellow-100 text-yellow-700' :
                                             product.orderStatus === 'Shipped' ? 'bg-blue-100 text-blue-700' :
                                                 product.orderStatus === 'Delivered' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'
                                         }`}>
                                         {product.orderStatus}
                                     </span> */}
-                    <span className='bg-green-100 text-green-700 py-1 px-2 shadow rounded text-sm font-medium'>Paid</span>
-                    <span className='border border-yellow-300 rounded-full text-yellow-500 px-3 py-1 text-sm font-medium shadow'>In Progrss</span>
+                    {/* <span className='bg-green-100 text-green-700 py-1 px-2 shadow rounded text-sm font-medium'>{paymentStatus}</span> */}
+                    <span className='border border-yellow-300 rounded-full text-yellow-500 px-3 py-1 text-sm font-medium shadow'>{orderStatus}</span>
                 </div>
             </div>
 
