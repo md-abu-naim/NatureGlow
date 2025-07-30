91636cb7d7673dde50ee71c64ddceb53695bf73b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-30 22:23:11 +0600

    Product details page displayed related products

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,17 +1,19 @@
 
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { Link, useLoaderData, useNavigate, useParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const ProductDetails = () => {
+    const [relatedProduct, setRelatedProduct] = useState([])
     const { id } = useParams();
     const products = useLoaderData()
     const navigate = useNavigate()
+    console.log(relatedProduct);
 
     const product = products?.find(p => p.id == id)
-    const {name, image, description, features, price, category, status} = product || {}
+    const { name, image, description, features, price, category, status } = product || {}
 
     const handleAddToCart = (product) => {
         const axistingCart = JSON.parse(localStorage.getItem("cart")) || []
@@ -33,10 +35,10 @@ const ProductDetails = () => {
 
     useEffect(() => {
         axios.get('/Product.json')
-        .then(res => {
-            const products = res.data.find(p => p.category === category)
-            console.log(products);
-        })
+            .then(res => {
+                const products = res.data.filter(p => p.category === category)
+                setRelatedProduct(products)
+            })
     }, [category])
     return (
         <div className='px-4 md:px-16 py-10'>
@@ -70,6 +72,31 @@ const ProductDetails = () => {
                     <div className="mt-4 text-center">
                         <Link to="/shop" className="text-sm text-green-700 hover:underline">⬅ Back to Shop</Link>
                     </div>
+                </div>
+            </div>
+            <div className='mt-10'>
+                <h3 className='text-2xl text-green-800 font-bold'>Related Products</h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 pt-4">
+                    {
+                        relatedProduct?.map(product => <div to={`/details/${product.id}`} key={product.id} className="group shadow-xl rounded-3xl p-5 relative hover:shadow-2xl transition duration-300">
+                            <div className="overflow-hidden rounded-xl">
+                                <img className="w-full h-52 object-contain rounded-xl group-hover:scale-105 transition-transform duration-300" src={product.image} alt={product.name} />
+                            </div>
+                            <div className="space-y-2 mt-4">
+                                <h3 className="text-lg font-bold text-green-500 transition">{product.name}</h3>
+                                <p className="text-sm italic group-hover:text-green-500">{product.category}</p>
+                                <p className="text-gray-700 text-sm">{product.shortBio}</p>
+                                <div className="flex items-center justify-between pt-2">
+                                    <span className="text-green-500 font-bold text-md">${product.price}</span>
+                                    <span className="text-sm bg-green-100 text-green-500 p-1.5 rounded-lg">{product.status}</span>
+                                </div>
+                                <div className="flex gap-3 w-full pt-3">
+                                    <Link to={`/details/${product.id}`} className="flex-1 px-4 py-2 rounded-full text-sm font-medium border border-green-500 text-green-600 hover:bg-green-100 transition">View Details</Link>
+                                    <Link to={`/checkout/${product.id}`} className="flex-1 px-4 py-2 rounded-full text-xm font-medium bg-green-500 text-white hover:bg-green-600 transition">Quick Buy</Link>
+                                </div>
+                            </div>
+                        </div>)
+                    }
                 </div>
             </div>
         </div>
