024412268237050ebbc175488dc08747460128b4
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-31 21:27:22 +0600

    order report chart

--- src/Pages/Dashboard/AdminBar/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminBar/AdminDashboard.jsx
@@ -7,7 +7,7 @@ import { LuPackagePlus } from "react-icons/lu";
 const AdminDashboard = () => {
     return (
         <div>
-            <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+            <section className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-6 p-4">
                 {/* Total Users */}
                 <div className="bg-white border border-green-200 rounded-2xl shadow-md hover:shadow-xl flex items-center gap-4 p-5 transition-all">
                     <div className="bg-green-100 text-green-700 p-3 rounded-full shadow-inner">
@@ -88,6 +88,11 @@ const AdminDashboard = () => {
                         <h3 className="text-2xl font-semibold text-green-800">522</h3>
                     </div>
                 </div>
+            </section>
+
+
+            <section>
+                
             </section>
         </div>
     );
