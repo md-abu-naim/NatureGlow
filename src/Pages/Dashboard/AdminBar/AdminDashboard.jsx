76eed572d2d3d3dd1bf7d86dd1d1ca7862b03f9b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-31 20:56:37 +0600

    admin dasboard page daily users

--- src/Pages/Dashboard/AdminBar/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminBar/AdminDashboard.jsx
@@ -4,15 +4,25 @@ import { FaUsers, FaShoppingCart, FaMoneyBillWave } from "react-icons/fa";
 const AdminDashboard = () => {
     return (
         <div>
-            <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+            <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
                 {/* Total Users */}
-                <div className="bg-white border border-green-300 rounded-2xl shadow-md hover:shadow-lg flex items-center gap-4 p-5 transition-all">
+                <div className="bg-white border border-green-200 rounded-2xl shadow-md hover:shadow-xl flex items-center gap-4 p-5 transition-all">
                     <div className="bg-green-100 text-green-700 p-3 rounded-full shadow-inner">
                         <FaUsers className="text-2xl" />
                     </div>
                     <div>
                         <p className="text-sm text-gray-500 font-medium">Total Users</p>
                         <h3 className="text-2xl font-semibold text-green-800">1522</h3>
+                    </div>
+                </div>
+                {/* Daily Users */}
+                <div className="bg-white border border-green-200 rounded-2xl shadow-md hover:shadow-xl flex items-center gap-4 p-5 transition-all">
+                    <div className="bg-green-100 text-green-700 p-3 rounded-full shadow-inner">
+                        <FaUsers className="text-2xl" />
+                    </div>
+                    <div>
+                        <p className="text-sm text-gray-500 font-medium">Daily Users</p>
+                        <h3 className="text-2xl font-semibold text-green-800">522</h3>
                     </div>
                 </div>
             </section>
