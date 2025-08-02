4efe9545d0c53ae227cc29aee773a1ba4b2132f5
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-02 20:22:16 +0600

    start composedChart

--- src/Pages/Dashboard/AdminBar/AdminDashboard/Charts.jsx
+++ src/Pages/Dashboard/AdminBar/AdminDashboard/Charts.jsx
@@ -157,50 +157,8 @@ export const StatusPieChart = ({ orders }) => {
     )
 }
 
-// const data = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: 'Page D',
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: 'Page E',
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: 'Page F',
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: 'Page G',
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
+
+// LineChart Start here
 export const StatusLineChart = ({ users }) => {
     const months = Array.from({ length: 12 }, (_, i) => {
         const date = subMonths(new Date(), 11 - i)
@@ -246,6 +204,20 @@ export const StatusLineChart = ({ users }) => {
                     </LineChart>
                 </ResponsiveContainer>
             </div>
+        </div>
+    );
+};
+
+
+// ComposedChart Start Here
+
+
+
+export const StatusComposedChart = () => {
+    return (
+        <div className="bg-white rounded-xl shadow p-4 h-[500px] sm:h-[350px] md:h-[500px]">
+            <h2 className='text-2xl font-bold text-green-800 pb-2'>Order Status Overview</h2>
+
         </div>
     );
 };
