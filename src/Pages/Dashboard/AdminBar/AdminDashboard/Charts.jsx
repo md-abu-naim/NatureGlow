79a3702a14e73c0b72fa46c7be63e76268447551
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-02 13:15:35 +0600

    linechart show > functionality compelete

--- src/Pages/Dashboard/AdminBar/AdminDashboard/Charts.jsx
+++ src/Pages/Dashboard/AdminBar/AdminDashboard/Charts.jsx
@@ -235,7 +235,6 @@ export const StatusLineChart = ({ users }) => {
                 <ResponsiveContainer width="100%" height="100%">
                     <LineChart
                         data={userStatusData}
-                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
