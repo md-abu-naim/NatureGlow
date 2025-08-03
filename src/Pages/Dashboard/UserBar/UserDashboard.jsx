27f2d54b5e0a73664576d03a9bf6ae752dff7bcb
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-03 21:01:24 +0600

    userDashboard > Total In Progress showing

--- src/Pages/Dashboard/UserBar/UserDashboard.jsx
+++ src/Pages/Dashboard/UserBar/UserDashboard.jsx
@@ -77,7 +77,7 @@ const UserDashboard = () => {
                     </div>
                     <div>
                         <p className="text-sm text-gray-500 font-medium">Total In Progress</p>
-                        <h3 className="text-2xl font-semibold text-green-800">{totalCancelled}</h3>
+                        <h3 className="text-2xl font-semibold text-green-800">{totalInProgresss}</h3>
                     </div>
                 </div>
                 {/* Total Shipped */}
