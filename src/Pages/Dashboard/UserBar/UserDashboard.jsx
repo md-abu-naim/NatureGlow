0c5fb9d7893c12e873073b38c00c07e847a6523b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-03 21:11:22 +0600

    userDashboard > This Week's Orders List

--- src/Pages/Dashboard/UserBar/UserDashboard.jsx
+++ src/Pages/Dashboard/UserBar/UserDashboard.jsx
@@ -1,8 +1,9 @@
+import { AiOutlineEye } from "react-icons/ai";
 import { BsBan, BsTruck } from "react-icons/bs";
 import { FaMoneyBillWave, FaShippingFast, FaShoppingCart } from "react-icons/fa";
 import { FiRefreshCw, FiTrendingUp } from "react-icons/fi";
 import { LuPackagePlus } from "react-icons/lu";
-import { useLoaderData } from "react-router-dom";
+import { NavLink, useLoaderData } from "react-router-dom";
 
 const UserDashboard = () => {
     const orders = useLoaderData()
@@ -16,7 +17,7 @@ const UserDashboard = () => {
     const totalInProgresss = orders?.filter(order => order.orderStatus === "In Progress")?.length
     const totalSipped = orders?.filter(order => order.orderStatus === "Shipped")?.length
     const totalCancelled = orders?.filter(order => order.orderStatus === "Cancelled")?.length
-    
+
     return (
         <div>
             <section className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-6 p-4">
@@ -100,6 +101,62 @@ const UserDashboard = () => {
                         <h3 className="text-2xl font-semibold text-green-800">{totalCancelled}</h3>
                     </div>
                 </div>
+            </section>
+
+
+
+
+            {/* This Week's Orders List */}
+            <section className="mt-7 overflow-x-auto rounded-lg shadow-lg border border-green-300">
+                <div className="bg-green-100 border-b flex justify-between items-center px-3 py-2 border-green-300">
+                    <h1 className="text-lg font-bold text-green-800">This Week's Orders List</h1>
+                    <NavLink to="/shop" className="bg-green-500 hover:bg-green-600 text-white py-1 px-2 rounded-lg transition">Shop Now</NavLink>
+                </div>
+                <table className="min-w-full text-sm text-left table-auto">
+                    <thead className="bg-green-100 text-green-800 font-semibold">
+                        <tr>
+                            <th className="py-3 px-4">#</th>
+                            <th className="py-3 px-4">Order ID</th>
+                            <th className="py-3 px-4">Products</th>
+                            <th className="py-3 px-4">Payment Status</th>
+                            <th className="py-3 px-4">Date</th>
+                            <th className="py-3 px-4">Order Status</th>
+                            <th className="py-3 px-4">Total Price</th>
+                            <th className="py-3 px-4 text-center">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-gray-700 divide-y divide-green-100">
+                        {
+                            dailyOrders?.map((order, i) => (
+                                <tr key={i} className="hover:bg-green-100 transition-all">
+                                    <td className="px-4 py-3 font-semibold">{i + 1}</td>
+                                    <td className="px-4 py-3 font-medium">{order._id}</td>
+                                    <td className="px-4 py-3 ">{order.products?.length} Items</td>
+                                    <td className="px-4 py-3 font-semibold">
+                                        <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm ${order.paymentStatus === 'Paid' ? 'bg-green-100 text-green-700' : order.paymentStatus === 'Unpaid' ? 'bg-red-100 text-red-700' : 'bg-blue-100 text-blue-700'}`}>
+                                            {order.paymentStatus}
+                                        </span>
+                                    </td>
+                                    <td className="px-4 py-3 font-sans font-semibold"> {order.date}</td>
+                                    <td className="px-4 py-3 font-semibold">
+                                        <span className={`px-2 py-1 rounded text-sm font-medium shadow-sm
+                                            ${order.orderStatus === 'In Progress' ? 'bg-yellow-100 text-yellow-700' :
+                                                order.orderStatus === 'Shipped' ? 'bg-blue-100 text-blue-700' :
+                                                    order.orderStatus === 'Delivered' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'
+                                            }`}>
+                                            {order.orderStatus}
+                                        </span>
+                                    </td>
+                                    <td className="px-4 py-3 font-sans">$ {order.totalPrice + 1}</td>
+                                    <td className="px-6 py-4 flex items-center justify-center gap-4 text-green-600">
+                                        <NavLink to={`/dashboard/order-details/${order._id}`} title="View"><AiOutlineEye className="hover:text-green-800 transition text-2xl" /></NavLink>
+                                        <button title="Cancel Order" className="bg-red-100 hover:bg-red-300 text-red-700 px-2 py-1 rounded-sm transition">Cancel</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
             </section>
         </div>
     );
