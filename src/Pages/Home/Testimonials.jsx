a75ad9c661db10f7d0ee28a9ada1bf78a7937221
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-10 13:11:12 +0600

    home page testimonials data from db

--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -1,14 +1,15 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
+import useAxiosCommon from "../../Hooks/useAxiosCommon";
 
 const Testimonials = () => {
-const [testimonials, setTestimonials] = useState([])
+    const [testimonials, setTestimonials] = useState([])
+    const axiosCommon = useAxiosCommon()
 
     useEffect(() => {
-        axios.get('/Testimonials.json')
-        .then(res => setTestimonials(res.data))
-    },[])
+        axiosCommon.get('/reviews')
+            .then(res => setTestimonials(res.data?.slice(0, 3)))
+    }, [axiosCommon])
     return (
         <section className="px-5">
             <div className="w-fit mx-auto border-x-4 text-center border-green-500 px-6 py-2 rounded-md">
@@ -18,17 +19,17 @@ const [testimonials, setTestimonials] = useState([])
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-7 gap-8">
                 {
-                    testimonials?.map(t => <div key={t.id} className="flex flex-col justify-between bg-green-50 border border-green-100 p-6 rounded-2xl shadow-sm hover:shadow-md transition duration-300">
-                        <p className="text-gray-700 italic mb-4"> “{t.message}”</p>
+                    testimonials?.map(t => <div key={t._id} className="flex flex-col justify-between bg-green-50 border border-green-100 p-6 rounded-2xl shadow-sm hover:shadow-md transition duration-300">
+                        <p className="text-gray-700 italic mb-4"> “{t.review}”</p>
                         <div className="flex gap-1 mb-6 text-yellow-500">
                             {
-                                [...Array(5)].map((_, i) => (
+                                [...Array(t.rating)].map((_, i) => (
                                     <FaStar key={i} className={i < t.rating ? "text-yellow-400" : "text-gray-300"} />
                                 ))
                             }
                         </div>
                         <div className="flex items-center gap-4 mt-auto">
-                            <img className="w-12 h-12 rounded-full object-center border-2 border-green-400" src={t.image} alt="" />
+                            <img className="w-12 h-12 rounded-full object-center border-2 border-green-400" src={t.profile} alt="" />
                             <div>
                                 <h4 className="font-semibold text-green-800">{t.name}</h4>
                                 <p className="text-sm text-gray-500">{t.title}</p>
