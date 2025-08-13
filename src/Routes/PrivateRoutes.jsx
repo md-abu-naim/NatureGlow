520aefbd074aaa3b04c0eaf6905240816ebd68e1
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-13 13:38:17 +0600

    redmi file setup

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,12 +1,12 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
 
-const PrivateRoutes = ({children}) => {
-    const {user, loading} = useAuth()
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useAuth()
     const location = useLocation()
-    
-    if(loading) return <h3 className="text-xl text-green-800">Loading...</h3>
-    if(user) return children;
+
+    if (loading) return <h3 className="text-xl text-green-800">Loading...</h3>
+    if (user) return children;
     return <Navigate to='/signIn' state={location.pathname} replace={true} />
 };
 
