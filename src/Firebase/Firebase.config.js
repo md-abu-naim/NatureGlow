f7b9515628b2609738f82d57196f8e337402fac0
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-15 11:23:32 +0600

    formate firebase

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -7,7 +7,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
