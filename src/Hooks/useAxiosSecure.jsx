63026dbd1277a9a2c793f1622704d181fc641329
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-16 20:30:55 +0600

    update useAxioseSecure

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,33 +1,3 @@
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-// import useAuth from "./useAuth";
-
-// const axiosSecure = axios.create({
-//     baseURL: 'https://natureglow.vercel.app',
-//     // baseURL: "http://localhost:3000",
-//     withCredentials: true
-// })
-
-// const useAxiosSecure = () => {
-//     const { LogoutUser } = useAuth();
-//     const navigate = useNavigate()
-
-//     axiosSecure.interceptors.response.use(res => {
-//         return res;
-//     },
-//         error => {
-//             if (error.response.status === 401 || error.response.status === 403) {
-//                 LogoutUser()
-//                     .then(() => navigate('/signIn'))
-//             }
-//             return Promise.reject(error)
-//         }
-//     )
-//     return axiosSecure
-// };
-
-// export default useAxiosSecure;
-
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
